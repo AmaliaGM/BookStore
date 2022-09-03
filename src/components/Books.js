3297f99ac863b3d7e8db84cc30e65735b3c97884
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-09-02 23:01:00 -0300

    book styling

--- src/components/Books.js
+++ src/components/Books.js
@@ -3,14 +3,15 @@ import AddNewBook from './BookInput';
 
 const Books = () => {
   return (
-   <div>
-    <div>
-
-      <div>
-        <span className='Author'>Mark Twain</span>
+   <div className='container'>
+    <div className='mediumCont'>
+     <div className='smallCont'>    
+      <div className='Book'>
+        <span className='Categorie'>Adventures</span>
         <span className='BookTitle'> Huckleberry Finn Adventures</span>
+        <span className='Author'>Mark Twain</span>
       </div>
-      <div>
+      <div className='TitleFoot'>
         <span className='Comments'>
           Comments
         </span>
@@ -19,23 +20,28 @@ const Books = () => {
         </span>
         <span>
           Edit
-        </span>
-      </div>
-      <div>
+        </span> 
+        </div>
+    </div>
+    <div className='progress'>
         <span>
           {/* <CircularProgressbar value{...percentage} /> */}
         </span>
+        <div className='percentage'>
         <span>
           64%
         </span>
         <span>
           completed
         </span>
+        </div>
       </div>
+      </div>
+      <div className='AddNewBook'>
       <AddNewBook />
-        
-    </div>
+      </div>
   </div>
+
   )
 }
 
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Categories = () => {
   return (
-    <div>Categories</div>
+    <div>Categories
+      <button type="button">CHECK STATUS</button>
+    </div>
   )
 }
 
