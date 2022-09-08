ba9d6cfe164974950f4ab61c23d016517f698926
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-09-08 15:15:46 -0300

    rephrase configureStore, rename delete reducer

--- src/Redux/Book/reducer.js
+++ src/Redux/Book/reducer.js
@@ -1,20 +1,20 @@
 const ADD_BOOKS = 'bookstore/books/ADD_BOOKS';
-const FILTER_BOOKS = 'bookstore/books/FILTER_BOOKS';
+const DELETE_BOOK = 'bookstore/books/DELETE_BOOK';
 
 export const addBook = (payload) => ({
   type: ADD_BOOKS,
   payload,
 });
-export const filterBooks = (payload) => ({
-  type: FILTER_BOOKS,
+export const deleteBook = (payload) => ({
+  type: DELETE_BOOK,
   payload,
 });
 const initialState = [];
 
 const bookReducer = (state = initialState, action) => {
   switch (action.type) {
-    case FILTER_BOOKS:
-      return { ...state, filterBooks: action.filter };
+    case DELETE_BOOK:
+      return { ...state, deleteBook: action.filter };
     case ADD_BOOKS:
       return [...state, action.payload];
     default:
--- src/Redux/ConfigureStore.js
+++ src/Redux/ConfigureStore.js
@@ -1,13 +1,12 @@
-import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import bookReducer from './Book/reducer';
 import categoryReducer from './Categories/categories';
 
-const reducers = combineReducers({
-  bookReducer,
-  categoryReducer,
+const store = configureStore({
+  reducer: {
+    bookReducer,
+    categoryReducer,
+  },
 });
-
-const store = configureStore(reducers);
 
 export default store;
