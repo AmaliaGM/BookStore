151988e5b0f0320f7e753688b6d3b161e1a84ed4
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-09-08 14:09:22 -0300

    reducers

--- src/Redux/Book/reducer.js
+++ src/Redux/Book/reducer.js
@@ -1,13 +1,20 @@
 const ADD_BOOKS = 'bookstore/books/ADD_BOOKS';
+const FILTER_BOOKS = 'bookstore/books/FILTER_BOOKS';
 
 export const addBook = (payload) => ({
   type: ADD_BOOKS,
   payload,
 });
-
+export const filterBooks = (payload) => ({
+  type: FILTER_BOOKS,
+  payload,
+});
 const initialState = [];
+
 const bookReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FILTER_BOOKS:
+      return { ...state, filterBooks: action.filter };
     case ADD_BOOKS:
       return [...state, action.payload];
     default:
